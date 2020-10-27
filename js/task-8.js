de5a253229505424b50eb7602eccf4cfbcb50607
Smekalin Sergei <crypto.mnr@gmail.com>
2020-10-27 16:01:40 +0200

    Добавил решение 8й задачи от ментора

--- js/task-8.js
+++ js/task-8.js
@@ -5,40 +5,76 @@ const refs = {
   boxes: document.getElementById('boxes'),
 };
 
-refs.createDivBtn.addEventListener('click', onCreateDivBtnClick);
+// refs.createDivBtn.addEventListener('click', onCreateDivBtnClick);
+// refs.resetDivBtn.addEventListener('click', destroyBoxes);
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+
+// function createDivs(amount) {
+//   const divArrayEl = [];
+//   divArrayEl.length = amount;
+
+//   let width = 30;
+//   let height = 30;
+//   const step = 10;
+
+//   for (let i = 1; i <= divArrayEl.length; i += 1) {
+//     let divEl = document.createElement('div');
+//     const red = getRandomInt(255);
+//     const green = getRandomInt(255);
+//     const blue = getRandomInt(255);
+
+//     divEl.style.width += `${width}px`;
+//     divEl.style.height += `${height}px`;
+//     width += step;
+//     height += step;
+
+//     divEl.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+//     refs.boxes.appendChild(divEl);
+//   }
+// }
+
+// function onCreateDivBtnClick() {
+//   createDivs(refs.inputField.value);
+// }
+
+// function destroyBoxes() {
+//   refs.inputField.value = '';
+//   refs.boxes.innerHTML = '';
+// }
+
+//-решение ментора
+const randomColor = () => {
+  const r = Math.round(Math.random() * 256);
+  const g = Math.round(Math.random() * 256);
+  const b = Math.round(Math.random() * 256);
+
+  return `rgb(${r},${g},${b})`;
+};
+
+const INITIAL_SIZE = 30;
+const STEP = 10;
+
+refs.createDivBtn.addEventListener('click', onCreateDivBtnClick(Number(refs.inputField.value)));
 refs.resetDivBtn.addEventListener('click', destroyBoxes);
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function onCreateDivBtnClick(amount) {
+  const boxesEl = [...new Array(amount)];
+  const arrBoxes = boxesEl.map((_, i) => createBox(i));
+
+  return boxes.append(...arrBoxes);
 }
 
-function createDivs(amount) {
-  const divArrayEl = [];
-  divArrayEl.length = amount;
+function createBox(i) {
+  const box = document.createElement('div');
+  box.style.backgroundColor = randomColor();
+  box.style.height = `${i * STEP + INITIAL_SIZE}px`;
+  box.style.width = `${i * STEP + INITIAL_SIZE}px`;
 
-  let width = 30;
-  let height = 30;
-  const step = 10;
-
-  for (let i = 1; i <= divArrayEl.length; i += 1) {
-    let divEl = document.createElement('div');
-    const red = getRandomInt(255);
-    const green = getRandomInt(255);
-    const blue = getRandomInt(255);
-
-    divEl.style.width += `${width}px`;
-    divEl.style.height += `${height}px`;
-    width += step;
-    height += step;
-
-    divEl.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-
-    refs.boxes.appendChild(divEl);
-  }
-}
-
-function onCreateDivBtnClick() {
-  createDivs(refs.inputField.value);
+  return box;
 }
 
 function destroyBoxes() {
